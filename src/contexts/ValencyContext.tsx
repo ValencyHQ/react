03f5cf22857c931b9b89cf81b32cc3dc98953da8
ahkohd <victor.olorunbunmi@gmail.com>
2021-05-10 23:34:23 +0100

    fix: handle the possibility that ValencyContext value can be  undefined

--- src/contexts/ValencyContext.tsx
+++ src/contexts/ValencyContext.tsx
@@ -1,8 +1,6 @@
 import { createContext } from 'react'
 import Valency from '@valencyhq/valency'
 
-const ValencyContext = createContext<Valency>(
-      new Valency({ uid: '', project: '', library: '' })
-)
+const ValencyContext = createContext<Valency | undefined>(undefined)
 
 export default ValencyContext
--- src/hooks/useValency.ts
+++ src/hooks/useValency.ts
@@ -4,12 +4,19 @@ import ValencyContext from '../contexts/ValencyContext'
 const useValency = () => {
       const valencyContext = useContext(ValencyContext)
 
-      return {
-            get: valencyContext.get,
-            getConfig: valencyContext.getConfig,
-            setConfig: valencyContext.setConfig,
-            asset: valencyContext.asset,
-      }
+      return valencyContext
+            ? {
+                    get: valencyContext.get,
+                    getConfig: valencyContext.getConfig,
+                    setConfig: valencyContext.setConfig,
+                    asset: valencyContext.asset,
+              }
+            : {
+                    get: () => undefined,
+                    getConfig: () => undefined,
+                    setConfig: () => undefined,
+                    asset: undefined,
+              }
 }
 
 export default useValency
