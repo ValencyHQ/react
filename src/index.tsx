3f4164067c5e3faf3ae67b5d91564a553fd6f3e1
ahkohd <victor.olorunbunmi@gmail.com>
2021-05-10 17:47:57 +0100

    fix: Export APIs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
-import './contexts/ValencyContext'
-import './hooks/useValency'
-import './components/Image'
-import './components/Object'
-import './components/Icon'
+import ValencyContext from 'contexts/ValencyContext'
+import useValency from 'hooks/useValency'
+import Image, { ImageProps } from 'components/Image'
+import Object, { ObjectProps } from 'components/Object'
+import Icon, { IconProps } from 'components/Icon'
+
+export {
+      ValencyContext,
+      useValency,
+      Image,
+      ImageProps,
+      Object,
+      ObjectProps,
+      Icon,
+      IconProps,
+}
