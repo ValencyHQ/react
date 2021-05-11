9a556495590786add0815cb1cf33f6a5c4340a5b
ahkohd <victor.olorunbunmi@gmail.com>
2021-05-11 19:10:42 +0100

    fix: revert

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import Image, { ImageProps } from './components/Image'
 import Object, { ObjectProps } from './components/Object'
 import Icon, { IconProps } from './components/Icon'
 
-export default ValencyContext
-
-export { useValency, Image, ImageProps, Object, ObjectProps, Icon, IconProps }
+export {
+      ValencyContext,
+      useValency,
+      Image,
+      ImageProps,
+      Object,
+      ObjectProps,
+      Icon,
+      IconProps,
+}
