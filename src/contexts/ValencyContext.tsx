2a65575cad60f54e122ac38167cf537c5636210d
ahkohd <victor.olorunbunmi@gmail.com>
2021-05-12 00:27:45 +0100

    feat: Implement ValencyProvider

--- src/contexts/ValencyContext.tsx
+++ src/contexts/ValencyContext.tsx
@@ -1,8 +1,24 @@
-import { createContext } from 'react'
+import React, { createContext, FC, ReactNode } from 'react'
 import Valency from '@valencyhq/valency'
 
-const ValencyContext = createContext<Valency>(
+export const ValencyContext = createContext<Valency>(
       new Valency({ uid: '', project: '', library: '' })
 )
 
-export default ValencyContext
+export interface ValencyProviderProps {
+      children: ReactNode
+      valency: Valency
+}
+
+const ValencyProvider: FC<ValencyProviderProps> = ({
+      children,
+      valency,
+}: ValencyProviderProps) => {
+      return (
+            <ValencyContext.Provider value={valency}>
+                  {children}
+            </ValencyContext.Provider>
+      )
+}
+
+export default ValencyProvider
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import ValencyContext from './contexts/ValencyContext'
+import ValencyProvider, {
+      ValencyContext,
+      ValencyProviderProps,
+} from './contexts/ValencyContext'
 import useValency from './hooks/useValency'
 import Image, { ImageProps } from './components/Image'
 import Object, { ObjectProps } from './components/Object'
 import Icon, { IconProps } from './components/Icon'
 
 export {
+      ValencyProvider,
+      ValencyProviderProps,
       ValencyContext,
       useValency,
       Image,
